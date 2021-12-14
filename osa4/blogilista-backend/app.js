d567d05c11c946f790f464cf0587e56f9fd1c92f
Rauno <raunorauno420@gmail.com>
2021-12-14 16:52:52 +0200

    tehty 4.15.-4.17.

--- osa4/blogilista-backend/app.js
+++ osa4/blogilista-backend/app.js
@@ -3,15 +3,21 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
-const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
